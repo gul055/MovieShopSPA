650b57deb876709063101ae87a626bf477ca36a5
gul055 <33allendelon@Gmail.com>
2020-12-17 11:19:45 -0800

    12/17/2020 finishMovieDetail

--- src/app/shared/models/movie.ts
+++ src/app/shared/models/movie.ts
@@ -1,6 +1,34 @@
 export interface Movie {
-    id: number;
-    title: string;
-    posterUrl: string;
-    releaseDate: string;
-  }
+  id: number;
+  title: string;
+  posterUrl: string;
+  backdropUrl: string;
+  rating?: any;
+  overview: string;
+  tagline: string;
+  budget: number;
+  revenue: number;
+  imdbUrl: string;
+  tmdbUrl: string;
+  releaseDate: string;
+  runTime: number;
+  price: number;
+  favoritesCount: number;
+  casts: Cast[];
+  genres: Genre[];
+}
+
+export interface Genre {
+  id: number;
+  name: string;
+  movieGenres?: any;
+}
+
+export interface Cast {
+  id: number;
+  name: string;
+  gender: string;
+  tmdbUrl: string;
+  profilePath: string;
+  character: string;
+}
